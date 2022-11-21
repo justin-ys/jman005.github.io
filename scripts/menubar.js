c5b79fe9dd2e6fe6492ae60883754e020a060517
Justin <justin106@protonmail.com>
2022-11-20 20:29:49 -0500

    Actually make modular menubar

---
+++ scripts/menubar.js
@@ -0,0 +1,12 @@
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "menubar.html", true);
+xhr.setRequestHeader('Content-type', 'text/html');
+xhr.onreadystatechange= function() {
+    if (this.readyState!==4) return;
+    if (this.status!==200) {
+		document.getElementById('menubar').innerHTML = "Whoops! Looks like there was a problem loading the Menu Bar...";
+	}
+    document.getElementById('menubar').innerHTML = this.responseText;
+	console.log(document.getElementById('menubar').innerHTML);
+};
+xhr.send();
