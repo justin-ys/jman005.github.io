26d9a6eb5743ab20f33c91664ec912ea60c4cd0d
BuildTools <justin106@protonmail.com>
2022-12-29 20:20:24 -0500

    Fix menubar stretching

--- scripts/menubar.js
+++ scripts/menubar.js
@@ -6,7 +6,7 @@ xhr.onreadystatechange= function() {
 		document.getElementById('menubar').innerHTML = "Whoops! Looks like there was a problem loading the Menu Bar...";
 	}
     document.getElementById('menubar').innerHTML = this.responseText;
-	document.getElementById('menubar').setAttribute("style", "width: 22em; height: 100vh; border-right: 1px solid black; background-color: #d3d6db;");
+	document.getElementById('menubar').setAttribute("style", "width: 22em; border-right: 1px solid black; margin-bottom: 1px; background-color: #d3d6db;");
 	console.log(document.getElementById('menubar').innerHTML);
 };
 xhr.send();
